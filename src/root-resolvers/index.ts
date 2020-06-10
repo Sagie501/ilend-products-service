e42162d27f2c53792bf09a3e1c7d3ec8df3c8102
Sagie <Sagie012@gmail.com>
2020-06-10 15:38:23 +0300

    Update dependencies

--- src/root-resolvers/index.ts
+++ src/root-resolvers/index.ts
@@ -1,6 +1,6 @@
-import * as _ from 'lodash';
 import { resolvers as productsResolver } from '../entities/product/product.resolvers';
 import { resolvers as categoryResolver } from '../entities/category/category.resolvers';
 import { resolvers as commentResolver } from '../entities/comment/comment.resolvers';
+import { mergeResolvers } from '@graphql-tools/merge';
 
-export const rootResolvers = _.merge(productsResolver, categoryResolver, commentResolver);
+export const rootResolvers = mergeResolvers([productsResolver, categoryResolver, commentResolver]);
--- src/root-schema/index.ts
+++ src/root-schema/index.ts
@@ -1,9 +1,14 @@
 import { gql } from 'apollo-server-express';
-import { mergeTypes } from 'merge-graphql-schemas';
 import { typeDefs as productsTypeDefs } from '../entities/product/product.schema';
 import { typeDefs as categoryTypeDefs } from '../entities/category/category.schema';
 import { typeDefs as commentTypeDefs } from '../entities/comment/comment.schema';
 import { typeDefs as scalarsTypeDefs } from 'graphql-scalars';
+import { mergeTypeDefs } from '@graphql-tools/merge';
 
-
-export const rootTypeDefs = gql`${mergeTypes([gql`${scalarsTypeDefs}`, categoryTypeDefs, productsTypeDefs, commentTypeDefs])}`;
+export const rootTypeDefs = gql`${mergeTypeDefs([gql`${scalarsTypeDefs}`, categoryTypeDefs, productsTypeDefs, commentTypeDefs], {
+  useSchemaDefinition: true,
+  forceSchemaDefinition: true,
+  throwOnConflict: true,
+  commentDescriptions: true,
+  reverseDirectives: true
+})}`;
